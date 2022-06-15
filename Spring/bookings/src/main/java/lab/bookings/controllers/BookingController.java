a36e0e5c7f1e9cb439d5eddec5239d87a1e1a162
mierzej <44375406+MierzeJevsky@users.noreply.github.com>
2022-06-15 21:09:03 +0200

    Take 6 UPDATE

--- Spring/bookings/src/main/java/lab/bookings/controllers/BookingController.java
+++ Spring/bookings/src/main/java/lab/bookings/controllers/BookingController.java
@@ -7,10 +7,7 @@ import lab.bookings.repositiores.BookingRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.time.LocalDate;
@@ -58,6 +55,11 @@ public class BookingController {
             }
         }
         return view;
+    }
+    @RequestMapping(value = "delete", method = RequestMethod.POST)
+    private String delete(@RequestParam long id){
+        bookingRepository.deleteById(id);
+        return "redirect:/bookings";
     }
 
     
